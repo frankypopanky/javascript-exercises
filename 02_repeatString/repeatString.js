ba9ebdf683f2e7a6cdddf32a1ba21d954ef80366
Frank H <81592635+frankypopanky@users.noreply.github.com>
2022-03-19 23:39:59 +0100

    jjjj

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    if (number < 0) {
+        let value = new Array();
+        value.push('ERROR');
+        return value.toString();
+    }
+    else{
+        let value=new Array();
+        for(i=0;i<number;i++){
+            value.push(string); 
+         }
+        return value.join('');
+    }
 
-};
-
+  };
 // Do not edit below this line
 module.exports = repeatString;
