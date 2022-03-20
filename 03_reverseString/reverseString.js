d1cc9f04b25fa96c6bc4d2bd058f164af8b8aa53
Frank H <81592635+frankypopanky@users.noreply.github.com>
2022-03-20 17:26:58 +0100

    done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(value) {
+    let valueLength = value.length -1;
+    let valueToReverse = new Array();
+    for(i = valueLength; i >= 0; i--) {
+        valueToReverse.push(value.charAt(i));
+    }
+    
+    return valueToReverse.join('');
 };
 
 // Do not edit below this line
