42b95172faabbe43824388679f663fbcb9085900
Frank H <81592635+frankypopanky@users.noreply.github.com>
2022-03-22 21:40:07 +0100

    sucessful

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
 const removeFromArray = function() {
+    let arrAllParams = Array.from(arguments);
+    let arrFirstParam = arrAllParams.shift();
+    let valueForDeletion = arrAllParams;
+    let itemValid = new Array();
 
+    for (i=0;i < arrFirstParam.length; i++){
+
+        if(valueForDeletion.includes(arrFirstParam[i])){
+         
+        }
+        else {
+        itemValid.push(arrFirstParam[i]);
+         
+        }
+         
+        
+    }
+    return itemValid;     
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
